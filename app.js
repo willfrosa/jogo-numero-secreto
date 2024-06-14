f9437e657dd889817cf503ea1371b4127c26d6fa
William Flores <williamfrosa@hotmail.com>
2024-06-14 09:42:41 -0300

    Alterando o número máximo para 50

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 20;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -74,10 +74,3 @@ function reiniciarJogo() {
     mudarImagemNaTela('imagem-menina',0);
     document.getElementById('reiniciar').setAttribute('disabled', true)
 }
-
-
-
-
-
-
-
